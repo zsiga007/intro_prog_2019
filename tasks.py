b61f717e6087304c58f83a1e2d0aa7267c27c2e6
Kálmán, László <kalman.laszlo@nytud.mta.hu>
2020-02-12 12:20:18 +0100

    started setting tasks

---
+++ tasks.py
@@ -0,0 +1,26 @@
+## tasks remaining for `ask_player_and_move()`
+
+def get_player_moves( die1, die2 ):
+    """
+    Return a list of moves the given player wants to make
+    """
+    ## temporary:
+    return []
+
+def is_move_possible( board, player, fromPoint, toPoint ):
+    """
+    Checks if a given move is legitimate
+    (only makes sure that the target point contains
+    `player`'s checkers, no checker or exactly one of
+    opponent's checkers.
+    """
+    ## temporary:
+    return True
+
+def move( board, player, fromPoint, toPoint ):
+    """
+    Assuming that the move is legitimate, actually carry it out.
+    The `board` gets modified!
+    """
+
+
